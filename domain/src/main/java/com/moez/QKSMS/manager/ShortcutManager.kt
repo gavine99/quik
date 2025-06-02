ae402820ac77ce28a0b18f09f4b9b3eb7b54ac3b
gavine99 <github@xymail.tk>
2025-06-02 14:50:57 +1000

    Revert "Implementing shortcut utilities"
    This reverts commit a01982864c9a27e1c1174bc26ccc2f526287a8be.

--- domain/src/main/java/com/moez/QKSMS/manager/ShortcutManager.kt
+++ domain/src/main/java/com/moez/QKSMS/manager/ShortcutManager.kt
@@ -18,17 +18,10 @@
  */
 package dev.octoshrimpy.quik.manager
 
-import android.content.pm.ShortcutInfo
-import androidx.core.content.pm.ShortcutInfoCompat
-
 interface ShortcutManager {
 
     fun updateBadge()
 
     fun updateShortcuts()
-
-    fun getShortcut(threadId: Long): ShortcutInfoCompat?
-
-    fun reportShortcutUsed(threadId: Long)
 
 }
